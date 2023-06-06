9dcb6e93bbd13dd76c0de77ee3c7e089d0aee81f
Arun Singh Parihar <arunsinghparihar3219@gmail.com>
2023-06-06 15:31:47 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum; 
+
+    int a,b; 
+    cout<<"Enter the two integer numbers"<<endl; cin>>a>>b; 
+    sum= a+b; 
+    cout<<"The sum is "<<sum<<endl;
+} 
