58534c0deb549addc2f605d5753c871e6a26e317
Arun Singh Parihar <arunsinghparihar3219@gmail.com>
2023-06-07 12:18:44 +0530

    diff file addes

--- diff.cpp
+++ diff.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main(){
-    int a, b ,c ;
-    return 1;
+    int a, b; 
+    cout<<"Enter the two integer no. "<<endl;
+    cin>>a>>b; 
+    cout<<"The difference is :"<<(a-b)<<endl;
 }
